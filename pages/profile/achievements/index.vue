5295bfed2f0dc50effdb9cce87f34ae3044f61c5
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-11-14 12:36:58 +0300

    Рабочий запрос

--- pages/profile/achievements/index.vue
+++ pages/profile/achievements/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h1 class="border-b-2 pb-4 text-xl font-bold">Достижения</h1>
+    {{ data }}
   </div>
 </template>
 
 <script setup>
+const { data } = useFetch('http://localhost:3001/test')
+
 definePageMeta({
   layout: 'profile'
 })
