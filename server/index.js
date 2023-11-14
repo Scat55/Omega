5cbd8e0f126b8fc7e04817472e2eb128083ecafe
NikolyaVOLCKOV <96441256+NikolyaVOLCKOV@users.noreply.github.com>
2023-11-14 11:58:29 +0300

    as

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const app = express();
 
 
 app.use(express.json());
-app.get("/test","test":"rctfvygbuhnijmok")
+app.get('/test', (req, res) => {
+    res.send('Добро пожаловать на мой сервер!');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
