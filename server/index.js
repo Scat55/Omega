90af8fe5a8571bb875df076419556d8696875e9a
NikolyaVOLCKOV <96441256+NikolyaVOLCKOV@users.noreply.github.com>
2023-11-14 11:59:31 +0300

    правка

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.get('/test', (req, res) => {
     res.send('Добро пожаловать на мой сервер!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
