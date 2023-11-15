bd59424b98e33335fb8277c16b471c4d6671c40e
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-11-16 00:29:45 +0300

    Пофиксил отображение информации о пользователе

--- components/ProfileDetail.vue
+++ components/ProfileDetail.vue
@@ -2,21 +2,22 @@
   <div class="profile">
     <!-- TODO: Переделать через массив (Запрос Коля) -->
     <div v-for="(value, key, idx) of user">
-      <div v-if="idx === 1">
-        <div
-          v-for="item in value"
-          class="grid grid-cols-2"
-        >
-          <div>
-            <p class="mt-7">Имя: {{ item }} </p>
-            <p class="mt-7">Фамилия: </p>
-          </div>
-          <div>
-            <p class="mt-7">Кол-во решшенных задач</p>
-            <p class="mt-7">Баланс: <span class="font-bold pl-4">10</span> OmegaCoin</p>
-          </div>
-        </div>
+      <div
+        v-if="idx === 0"
+        class="grid grid-cols-2"
+      >
+        <div>
+          <!-- {{ value }} -->
+          <p class="mt-7 "><span class="font-bold">Имя:</span> {{ value.first_name }} </p>
+          <p class="mt-7 "><span class="font-bold">Фамилия:</span> {{ value.last_name }} </p>
+          <p class="mt-7 "><span class="font-bold">Почта:</span> {{ value.email }} </p>
 
+        </div>
+        <div>
+          <p class="mt-7 "><span class="font-bold">Тип:</span> {{ value.type_user }} </p>
+          <p class="mt-7">Кол-во решшенных задач</p>
+          <p class="mt-7">Баланс: <span class="font-bold pl-4">10</span> OmegaCoin</p>
+        </div>
       </div>
     </div>
     <div>
@@ -25,6 +26,20 @@
   </div>
 </template>
 
+<!-- <div
+          v-for="item in value"
+          
+        >
+          {{ item }}
+          <div>
+            <p class="mt-7">Имя: {{ item }} </p>
+            <p class="mt-7">Фамилия: </p>
+          </div>
+          <div>
+            <p class="mt-7">Кол-во решшенных задач</p>
+            <p class="mt-7">Баланс: <span class="font-bold pl-4">10</span> OmegaCoin</p>
+          </div>
+        </div> -->
 <script setup>
 const { user } = defineProps(['user'])
 
