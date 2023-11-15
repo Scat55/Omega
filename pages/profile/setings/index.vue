57e3f7536e2fbc0a0e7b3e0737292017c9c16fdd
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-11-15 10:23:50 +0300

    Сделал обрботку формы

--- pages/profile/setings/index.vue
+++ pages/profile/setings/index.vue
@@ -13,108 +13,141 @@
   ```
 -->
 <template>
-  <form>
-    <div class="space-y-12">
-      <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Настройки профиля</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">Эта информация будет отображаться публично, поэтому будьте
-          осторожны с тем, чем делитесь.</p>
+  <div class="container">
+    <form @submit.prevent="handlerForm">
+      <div class="space-y-12">
+        <div class="border-b border-gray-900/10 pb-12">
+          <h2 class="text-base font-semibold leading-7 text-gray-900">Настройки профиля</h2>
+          <p class="mt-1 text-sm leading-6 text-gray-600">Эта информация будет отображаться публично, поэтому будьте
+            осторожны с тем, чем делитесь.</p>
 
 
-      </div>
+        </div>
 
-      <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Персональная информация</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">Используйте постоянный адрес, по которому вы можете получать
-          почту.</p>
+        <div class="border-b border-gray-900/10 pb-12">
+          <h2 class="text-base font-semibold leading-7 text-gray-900">Персональная информация</h2>
+          <p class="mt-1 text-sm leading-6 text-gray-600">Используйте постоянный адрес, по которому вы можете получать
+            почту.</p>
 
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-3">
-            <label
-              for="first-name"
-              class="block text-sm font-medium leading-6 text-gray-900"
-            >Имя</label>
-            <div class="mt-2">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autocomplete="given-name"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
-              />
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="sm:col-span-3">
+              <label
+                for="first-name"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >Имя</label>
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autocomplete="given-name"
+                  v-model="userName"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                />
+              </div>
             </div>
+
+            <div class="sm:col-span-3">
+              <label
+                for="last-name"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >Фамилия</label>
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autocomplete="family-name"
+                  v-model="userLastName"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-4">
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >Адрес электронной почты</label>
+              <div class="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  disabled
+                  value="test@mail.ru"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >Класс</label>
+              <div class="mt-2">
+                <input
+                  id="class"
+                  name="class"
+                  type="text"
+                  autocomplete="class"
+                  v-model="userClass"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                />
+              </div>
+            </div>
+
+            <!-- <div class="col-span-full">
+              <label
+                for="photo"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >Фото</label>
+              <div class="mt-2 flex items-center gap-x-3">
+                <UserCircleIcon
+                  class="h-12 w-12 text-gray-300"
+                  aria-hidden="true"
+                />
+                <button
+                  type="button"
+                  class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >Изменить</button>
+              </div>
+            </div> -->
           </div>
-
-          <div class="sm:col-span-3">
-            <label
-              for="last-name"
-              class="block text-sm font-medium leading-6 text-gray-900"
-            >Фамилия</label>
-            <div class="mt-2">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autocomplete="family-name"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
-              />
-            </div>
-          </div>
-
-          <div class="sm:col-span-4">
-            <label
-              for="email"
-              class="block text-sm font-medium leading-6 text-gray-900"
-            >Адрес электронной почты</label>
-            <div class="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autocomplete="email"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
-              />
-
-
-            </div>
-          </div>
-
-
-          <!-- <div class="col-span-full">
-            <label
-              for="photo"
-              class="block text-sm font-medium leading-6 text-gray-900"
-            >Фото</label>
-            <div class="mt-2 flex items-center gap-x-3">
-              <UserCircleIcon
-                class="h-12 w-12 text-gray-300"
-                aria-hidden="true"
-              />
-              <button
-                type="button"
-                class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              >Изменить</button>
-            </div>
-          </div> -->
         </div>
       </div>
-    </div>
 
-    <div class="mt-6 flex items-center justify-end gap-x-6">
-      <button
-        type="button"
-        class="text-sm font-semibold leading-6 text-gray-900"
-      >Отмена</button>
-      <button
-        type="submit"
-        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >Сохранить</button>
-    </div>
-  </form>
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="reset"
+          class="text-sm font-semibold leading-6 text-gray-900"
+        >Отмена</button>
+        <button
+          type="submit"
+          class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >Сохранить</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { UserCircleIcon } from '@heroicons/vue/24/solid'
+
+const userName = ref('')
+const userLastName = ref('')
+
+const userClass = ref('')
+
+
+function handlerForm() {
+  console.log(userName.value, userLastName.value, userClass.value)
+  userName.value = userLastName.value = userClass.value = ''
+
+}
+
 definePageMeta({
   layout: 'profile'
 })
